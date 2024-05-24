114690f843e96d558fc06cb88c87c726daa1826a
ollieri3 <oliver.ridsdale@gmail.com>
2024-05-24 09:18:07 +0100

    Fix path to schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "/app/db/schema.ts",
+  schema: "app/db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
