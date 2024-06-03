fe18d158877088222805108e55ebe3eadfc300ef
ollieri3 <oliver.ridsdale@gmail.com>
2024-06-03 09:15:28 +0100

    Add Tailwind prettier plugin for class sorting

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -27,11 +27,11 @@ export async function loader() {
 export default function Index() {
   const { isDBConnected } = useLoaderData<typeof loader>();
   return (
-    <main className=" w-screen h-screen flex items-center justify-center bg-orange-100">
+    <main className="flex h-screen w-screen items-center justify-center bg-orange-100">
       <div className="flex flex-col items-center">
         <img src={bach} height={250} width={250} alt="" className="my-10" />
-        <h1 className="text-5xl bold font-serif">Welcome to the Bach Stack</h1>
-        <div className="my-10 text-sm text-gray-600 flex items-center gap-2">
+        <h1 className="bold font-serif text-5xl">Welcome to the Bach Stack</h1>
+        <div className="my-10 flex items-center gap-2 text-sm text-gray-600">
           <span>{isDBConnected ? "🟢" : "🔴"}</span>Database Connection
         </div>
       </div>
